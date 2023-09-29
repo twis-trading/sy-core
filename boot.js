2556c0234ba7c046e7a7f5ab0b7687efaa951a3b
john.nico.novero.wsi@prulifeuk.com.ph <john.nico.novero.wsi@prulifeuk.com.ph>
2023-09-29 17:32:04 +0800

    add dotenv

--- boot.js
+++ boot.js
@@ -2,12 +2,10 @@ import express from "express";
 const app = express();
 import bodyParser from "body-parser";
 import cors from 'cors'
+import dotenv from 'dotenv'
 const PORT = process.env.PORT || 3000;
 
-// import createSocketConnection from "./config/socket.config.js";
-// const socket = createSocketConnection("http://localhost:4000"); // Connect to the Socket.IO server (Server 2)
-
-import config from "./config/server.config.js";
+dotenv.config()
 import svcRoute from "./routes/svc.routes.js";
 app.use(cors({origin:"*"}))
 app.use(bodyParser.json());
@@ -16,19 +14,8 @@ app.get("/", (req, res) => {
 });
 
 svcRoute(app);
-// Example route to emit a message to the Socket.IO server
-// app.get("/emit-message", (req, res) => {
-//   const message = "Hello from Express server (Server 1)";
 
-//   // Emit the message to the Socket.IO server (Server 2)
-//   socket2.emit("message2", message);
-//   res.send("Message emitted to Socket.IO server (Server 2)");
-// });
 
-// socket.on("test_to_serverApi", (data) => {
-//   console.error("Android to SocketServer to Server Api: Success", data);
-//   socket.emit("serverApi_to_socketServer");
-// });
 
 app.listen(PORT, () => {
   console.log(`sy-core is listening on port ${PORT}`);
