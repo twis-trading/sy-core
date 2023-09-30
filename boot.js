3a962172cf327d2c51a07488fb7196c69361ac0f
john.nico.novero.wsi@prulifeuk.com.ph <john.nico.novero.wsi@prulifeuk.com.ph>
2023-09-30 12:51:32 +0800

    update cors config

--- boot.js
+++ boot.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 const svcRoute = require("./routes/svc.routes.js")
 
 const corsOptions = {
-  origin: ['http://45.76.157.112:4200',"http://localhost:5173"]
+  origin: ['http://45.76.157.112:4200',"http://localhost:5173","http://localhost:3000"]
 };
 
 app.use(bodyParser.json());
